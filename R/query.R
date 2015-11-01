f454d28140a2e8a5d2579edc6f57c6e541f3edef
ironholds <ironholds@gmail.com>
2015-10-31 22:16:17 -0400

    finish inline docs

--- R/query.R
+++ R/query.R
@@ -2,14 +2,45 @@
 #'@description retrieves the pageview data for a particular article on a project, within
 #'a provided time-range.
 #'
+#'@param project the name of the project, structured as \code{[language_code].[project]}
+#'(see the default).
+#'
+#'@param article the article(s) you want to retrieve data for. Ideally features underscores in the title
+#'instead of spaces, but happily converts if you forget to do this.
+#'
+#'@param platform The platform the pageviews came from; one of "all", "desktop", "mobile-web" and
+#'"mobile-app". Set to "all" by default.
+#'
+#'@param user_type the type of users. One of "all", "user", "spider" or "bot". "all" by default.
+#'
+#'@param start the start \code{YYYYMMDDHH} of the range you want to cover.
+#'
+#'@param end the end \code{YYYYMMDDHH} of the range you want to cover. NULL by default, meaning
+#'that it returns 1 day of data.
+#'
+#'@param reformat Whether to reformat the results as a \code{\link{data.frame}} or not. TRUE by default.
+#'
+#'@param ... further arguments to pass to httr's GET.
+#'
+#'@seealso \code{\link{top_articles}} for the top articles per project in a given date range,
+#'and \code{\link{project_pageviews}} for per-project pageviews.
+#'
+#'@examples
+#'# Basic example
+#'r_pageviews <- article_pageviews()
+#'
+#'# Modify the article
+#'obama_pageviews <- article_pageviews(article = "Barack_Obama")
+#'
 #'@export
-article_pageviews <- function(project = "en.wikipedia", article = "Barack Obama",
-                              platform = "all", user_type = "all", granularity = "daily",
-                              start = "2015100100", end = NULL, ...){
+article_pageviews <- function(project = "en.wikipedia", article = "R (programming language)",
+                              platform = "all", user_type = "all",
+                              start = "2015100100", end = NULL, reformat = TRUE, ...){
   
+  article <- gsub(x = article, pattern = " ", replacement = "_", fixed = TRUE)
   if(length(article) > 1){
     return(lapply(article, article_pageviews, project = project, platform = platform,
-                  user_type = user_type, granularity = granularity, start = start, end = end, ...))
+                  user_type = user_type, start = start, end = end, ...))
   }
   # Handle timestamps
   if(is.null(end)){
@@ -18,11 +49,20 @@ article_pageviews <- function(project = "en.wikipedia", article = "Barack Obama"
   
   # Construct parameters
   parameters <- paste("per-article", project, ifelse(platform == "all", "all-access", platform),
-                      ifelse(user_type == "all", "all-agents", user_type), article, granularity,
+                      ifelse(user_type == "all", "all-agents", user_type), article, "daily",
                       start, end, sep = "/")
   
   # Run and return
-  return(pv_query(parameters, ...)$items)
+  data <- pv_query(parameters, ...)$items
+  
+  if(reformat){
+    nameset <- names(data[[1]])
+    data <- data.frame(matrix(unlist(data), nrow = length(data), byrow = TRUE), stringsAsFactors = FALSE)
+    names(data) <- nameset
+    data$views <- as.numeric(data$views)
+    data <- data[,!names(data) == "granularity"]
+  }
+  return(data)
 }
 
 #'@title Retrieve Data on Top Articles
@@ -35,28 +75,86 @@ article_pageviews <- function(project = "en.wikipedia", article = "Barack Obama"
 #'@param platform The platform the pageviews came from; one of "all", "desktop", "mobile-web" and
 #'"mobile-app". Set to "all" by default.
 #'
-#'@param year The year the articles were "top" in. "all" by default; if it is "all", month
-#'and day must also be "all".
+#'@param year The year the articles were "top" in. 2015 by default.
 #'
-#'@param month The month the articles were "top" in. "all" by default; if it is "all", day must also
-#'be "all".
+#'@param month The month the articles were "top" in. "10" by default; can be set to "all", for all
+#'the months in \code{year}. If so, \code{day} must also be "all".
 #'
-#'@param day The day the articles were "top" in. "all" by default; this simply returns the top articles
-#'for the relevant month/year combination.
+#'@param day The day the articles were "top" in. "01" by default; can be set to "all", for all
+#'the days in \code{month}.
+#'
+#'@param reformat Whether to reformat the results as a \code{\link{data.frame}} or not. TRUE by default.
+#'
+#'@param ... further arguments to pass to httr's GET.
 #'
 #'@seealso \code{\link{article_pageviews}} for per-article pageviews and \code{\link{project_pageviews}} for
 #'per-project pageviews.
 #'
+#'@examples
+#'# Basic example
+#'enwiki_top_articles <- top_articles()
+#'
+#'# Use a narrower platform
+#'enwiki_mobile_top <- top_articles(platform = "mobile-web")
+#'
+#'@importFrom jsonlite fromJSON
 #'@export
-top_articles <- function(project = "en.wikipedia", platform = "all", year = "all",
-                         month = "all", day = "all", ...) {
+top_articles <- function(project = "en.wikipedia", platform = "all", year = "2015",
+                         month = "10", day = "01", reformat = TRUE, ...) {
   
   parameters <- paste("top", project, ifelse(platform == "all", "all-access", platform),
-                      ifelse(year == "all", "all-years", year), ifelse(year == "all", "all-years", year))
-  return(pv_query(parameters, ...)$items)
+                      year, ifelse(month == "all", "all-months", month),
+                      sep = "/", ifelse(day == "all", "all-days", day))
+  results <- pv_query(parameters, ...)$items
+  
+  if(reformat){
+    results <- do.call("rbind", lapply(results, function(x){
+      data <- jsonlite::fromJSON(x$articles)
+      data$project <- x$project
+      data$access <- x$access
+      data$year <- x$year
+      data$month <- x$month
+      data$day <- x$day
+      return(data)
+    }))
+  }
+  return(results)
 }
 
 #'@title Retrieve Per-Project Pageview Counts
+#'
+#'@description Retrieve pageview counts for a particular project.
+#'
+#'@param project the name of the project, structured as \code{[language_code].[project]}
+#'(see the default).
+#'
+#'@param platform The platform the pageviews came from; one of "all", "desktop", "mobile-web" and
+#'"mobile-app". Set to "all" by default.
+#'
+#'@param user_type the type of users. One of "all", "user", "spider" or "bot". "all" by default.
+#'
+#'@param granularity the granularity of data to return; do you want hourly or daily counts? Set
+#'to "daily" by default.
+#'
+#'@param start the start \code{YYYYMMDDHH} of the range you want to cover.
+#'
+#'@param end the end \code{YYYYMMDDHH} of the range you want to cover. NULL by default, meaning
+#'that it returns 1 day/hour of data (depending on the value passed to \code{granularity}).
+#'
+#'@param reformat Whether to reformat the results as a \code{\link{data.frame}} or not. TRUE by default.
+#'
+#'@param ... further arguments to pass to httr's GET.
+#'
+#'@examples
+#'# Basic call
+#'enwiki_1_october_pageviews <- project_pageviews()
+#'
+#'# Break it down to hourly
+#'enwiki_hourly <- project_pageviews(granularity = "hourly", end = "2015100123")
+#'
+#'@seealso \code{\link{top_articles}} for the top articles per project in a given date range,
+#'and \code{\link{article_pageviews}} for per-article pageviews.
+#'
 #'@export
 project_pageviews <- function(project = "en.wikipedia", platform = "all", user_type = "all",
                               granularity = "daily", start = "2015100100", end = NULL, reformat = TRUE,
@@ -81,8 +179,6 @@ project_pageviews <- function(project = "en.wikipedia", platform = "all", user_t
     data <- data.frame(matrix(unlist(data), nrow = length(data), byrow = TRUE), stringsAsFactors = FALSE)
     names(data) <- nameset
     data$views <- as.numeric(data$views)
-    return(data)
   }
-  
   return(data)
 }
--- tests/testthat/test.R
+++ tests/testthat/test.R
@@ -7,3 +7,17 @@ test_that("basic project pageview queries work", {
   expect_true(nrow(result) == 1)
   expect_true(ncol(result) == 6)
 })
+
+test_that("Basic top-article queries work", {
+  result <- top_articles()
+  expect_true(is.data.frame(result))
+  expect_true(nrow(result) == 1000)
+  expect_true(ncol(result) == 8)
+})
+
+test_that("Basic per-article queries work", {
+  result <- article_pageviews()
+  expect_true(is.data.frame(result))
+  expect_true(nrow(result) == 1)
+  expect_true(ncol(result) == 6)
+})
