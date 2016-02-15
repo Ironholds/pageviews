e2409a5a7860738fa02badbae274d037f2df662d
Jeremiah Lewis <jeremiah.lewis@razorfish.de>
2016-02-15 19:35:22 +0100

    build out class tests and update vignettes

--- R/utils.R
+++ R/utils.R
@@ -1,5 +1,5 @@
 #'@importFrom httr stop_for_status GET user_agent content status_code
-pv_query <- function(params, reformat = TRUE, ...){
+pv_query <- function(params, reformat, ...){
   # Run multiple queries, return as list of results
   data_list <- lapply(as.list(params), pv_query_single, reformat = reformat)
 
@@ -13,7 +13,7 @@ pv_query <- function(params, reformat = TRUE, ...){
 pv_query_single <- function(params, reformat, ...){
   url <- paste0("https://wikimedia.org/api/rest_v1/metrics/pageviews/", params)
 
-  result <- httr::GET(url, httr::user_agent("pageviews API client library - https://github.com/Ironholds/pageviews"))
+  result <- httr::GET(url, httr::user_agent("pageviews API client library - https://github.com  /Ironholds/pageviews"))
   # Check response success
   if(httr::status_code(result) == 404){
     stop(httr::content(result, type = "application/json")$detail)
@@ -34,7 +34,7 @@ reformat_data <- function(data){
   data <- data$items
 
   if("articles" %in% names(data[[1]])){ # Handle Top Articles formatting
-    result <- data.frame(do.call(rbind, data[[1]]$articles)
+    result <- as.data.frame(do.call(rbind, data[[1]]$articles)
                         , stringsAsFactors = FALSE)
     data[[1]]$articles <- NULL
     meta <- do.call(cbind, data[[1]])
@@ -47,19 +47,25 @@ reformat_data <- function(data){
       data["granularity"] <- "day"
     }
 
-    data$date <- as.Date(paste(data$year, data$month, data$day, sep = "-"))
+    data$date <- as.POSIXct(paste(data$year, data$month, data$day, sep = "-"))
     data$rank <- as.numeric(data$rank)
-
   } else {
-    data <- data.frame(do.call(rbind, data), stringsAsFactors = FALSE)
-
-    data$date <- as.Date(substr(data$timestamp, 1, 8), format = "%Y%m%d")
+    data <- as.data.frame(do.call(rbind, data), stringsAsFactors = FALSE)
+    data$date <- as.POSIXct(as.character(data$timestamp), format = "%Y%m%d%H", tz = "GMT")
   }
 
 
+  # Convert all variables to appropriate classes & formats.
   data$views <- as.numeric(data$views)
   data$language <- gsub("(.*)\\.(.*)", "\\1", data$project)
   data$project <- gsub("(.*)\\.(.*)", "\\2", data$project)
+  data$access <- as.character(data$access)
+  data$granularity <- as.character(data$granularity)
+
+  # Agent and article only contained in some queries
+  if(!is.null(data$agent)) data$agent <- as.character(data$agent)
+  if(!is.null(data$article)) data$article <- as.character(data$article)
+
 
   # Set Consistent Column Order
   col_order <- c("project", "language", "article", "access"
